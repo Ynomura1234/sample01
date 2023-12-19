ca9f868bea7aa8fbf349683cbbd6bf51d9e086f0
Ynomura1234 <y.nomura1234@gmail.com>
2023-12-19 15:16:43 +0900

    動作確認

---
+++ src/main/java/sample01/beans/Person.java
@@ -0,0 +1,43 @@
+package sample01.beans;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class Person {
+	private String firstName;
+	private String lastName;
+	private int age;
+
+	public Person(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
